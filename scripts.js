7e54fb683b3f29b590b857ba2d504f631639208c
duhnuker <dinukanh@gmail.com>
2023-03-23 10:51:47 +1100

    variables established, queryselects

---
+++ scripts.js
@@ -0,0 +1,17 @@
+let previousValue = "";
+let operator = "";
+let currentValue = "";
+
+const displayPreviousValue = document.querySelector("previousValue");
+const displayCurrentValue = document.querySelector("currentValue");
+
+const numberButtons = document.querySelectorAll(".numberButton");
+
+const decimal = document.querySelector(".decimalButton");
+
+const equal = document.querySelector(".equalButton");
+
+
+const clear = document.querySelector(".clearButton");
+
+const operators = document.querySelectorAll(".operator");
